f4b990a072dab6333bbe18c5c6c23b859d4e6ea8
juliatogr <julia.tortosa01@estudiant.upf.edu>
2022-10-25 23:46:33 +0200

    add audios

--- scripts/bbb_stereo_mono.py
+++ scripts/bbb_stereo_mono.py
@@ -1,9 +1,9 @@
 from pydub import AudioSegment
 
-sound = AudioSegment.from_file("../originals/bbb_stereo.wav")
-sound = sound.set_channels(1)
-sound.export("../results/bbb_mono.wav", format="wav")
-
 sound = AudioSegment.from_file("../originals/bbb_mono.wav")
 sound = sound.set_channels(2)
 sound.export("../results/bbb_stereo.wav", format="wav")
+
+sound = AudioSegment.from_file("../results/bbb_stereo.wav")
+sound = sound.set_channels(1)
+sound.export("../results/bbb_mono.wav", format="wav")
